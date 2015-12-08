7718969aace2a0241ff2a5b3378d4e53bfa58d27
Ben Pochily <ben.pochily@gmail.com>
2015-12-07 22:27:39 -0500

    force rebuild

--- server/config/seed.js
+++ server/config/seed.js
@@ -3,7 +3,7 @@
  * to disable, edit config/environment/index.js, and set `seedDB: false`
  */
 
-'use strict'; 
+'use strict';
 import Thing from '../api/thing/thing.model';
 import User from '../api/user/user.model';
 
