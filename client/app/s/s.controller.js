483a8bd3120510a2cc1d46fa4b08dbb70de771c9
Ben Pochily <ben.pochily@gmail.com>
2015-12-13 16:18:20 -0500

    problem type now configurable by Angular

--- client/app/s/s.controller.js
+++ client/app/s/s.controller.js
@@ -2,7 +2,7 @@
 
 angular.module('hrr10MjbeApp')
   .controller('SCtrl', function ($scope, Problems) {
-    Problems.setCurrentProblemSet(5);
+    Problems.setCurrentProblemSet(2);
     $scope.message = 'Hello';
     $scope.type = Problems.currentProblemSet();
   });
