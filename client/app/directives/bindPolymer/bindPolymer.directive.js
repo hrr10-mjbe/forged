88677732117d950806eb6cbc22d295dcc8175dec
Ben Pochily <ben.pochily@gmail.com>
2015-12-13 22:31:45 -0500

    cleanup

--- client/app/directives/bindPolymer/bindPolymer.directive.js
+++ client/app/directives/bindPolymer/bindPolymer.directive.js
@@ -1,34 +1,31 @@
 'use strict';
-
-//angular.module('hrr10MjbeApp')
-//angular.module('hrr10MjbeApp', ['bindPolymer']);
+//based on binding implementation by Josh Crowther
 angular.module('bindPolymer', [])
- .directive('bindPolymer', ['$parse', function($parse) {
-  console.log('in directive');
-      return {
-        restrict: 'A',
-        scope : false,
-        compile: function bindPolymerCompile(el, attr) {
-          var attrMap = {};
+  .directive('bindPolymer', ['$parse', function($parse) {
+    return {
+      restrict: 'A',
+      scope: false,
+      compile: function bindPolymerCompile(el, attr) {
+        var attrMap = {};
 
-          for (var prop in attr) {
-            if (angular.isString(attr[prop])) {
-              var _match = attr[prop].match(/\{\{\s*([\.\w]+)\s*\}\}/);
-              if (_match) {
-                attrMap[prop] = $parse(_match[1]);
-              }
+        for (var prop in attr) {
+          if (angular.isString(attr[prop])) {
+            var _match = attr[prop].match(/\{\{\s*([\.\w]+)\s*\}\}/);
+            if (_match) {
+              attrMap[prop] = $parse(_match[1]);
             }
           }
-          return function bindPolymerLink(scope, element, attrs) {
-            Object.keys(attrMap).forEach(function(key) {
-              element.on(key + '-changed', function(event) {
-                scope.$evalAsync(function() {
-                  if (attrMap[key](scope) === event.detail.value) return;
-                  attrMap[key].assign(scope, event.detail.value);
-                });
+        }
+        return function bindPolymerLink(scope, element, attrs) {
+          Object.keys(attrMap).forEach(function(key) {
+            element.on(key + '-changed', function(event) {
+              scope.$evalAsync(function() {
+                if (attrMap[key](scope) === event.detail.value) return;
+                attrMap[key].assign(scope, event.detail.value);
               });
             });
-          };
-        }
-      };
-    }]);
+          });
+        };
+      }
+    };
+  }]);
