8c44bd9e26cdeeddf39435a62dad8460e744b6e2
Ben Pochily <ben.pochily@gmail.com>
2015-12-14 00:38:43 -0500

    cleanup

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,40 +2,40 @@
 
 (function() {
 
-class MainController {
+  class MainController {
 
-  constructor($http, $state, Problems) {
-    this.$http = $http;
-    this.$state = $state;
-    this.Problems = Problems;
-    this.awesomeThings = [];
-    
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
-  }
+    constructor($http, $state, Problems) {
+      this.$http = $http;
+      this.$state = $state;
+      this.Problems = Problems;
+      this.awesomeThings = [];
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
+      $http.get('/api/things').then(response => {
+        this.awesomeThings = response.data;
+      });
     }
+
+    addThing() {
+      if (this.newThing) {
+        this.$http.post('/api/things', {
+          name: this.newThing
+        });
+        this.newThing = '';
+      }
+    }
+
+    deleteThing(thing) {
+      this.$http.delete('/api/things/' + thing._id);
+    }
+
+    go() {
+      this.Problems.setCurrentProblemSet(this.userselection);
+      this.$state.go('s');
+    }
+
   }
 
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
+  angular.module('hrr10MjbeApp')
+    .controller('MainController', MainController);
 
-  go() {
-    console.log('testing');
-    this.Problems.setCurrentProblemSet(this.userselection);
-    console.log(this.Problems.currentProblemSet(1));
-    this.$state.go('s');
-  }
-
-}
-
-angular.module('hrr10MjbeApp')
-  .controller('MainController', MainController);
-    
 })();
