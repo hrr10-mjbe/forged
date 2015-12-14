8fbf725b340897cc121b818b2c974175ca4be755
Ben Pochily <ben.pochily@gmail.com>
2015-12-14 00:35:37 -0500

    bugfix

--- client/app/s/s.controller.js
+++ client/app/s/s.controller.js
@@ -2,7 +2,6 @@
 
 angular.module('hrr10MjbeApp')
   .controller('SCtrl', function ($scope, Problems) {
-    Problems.setCurrentProblemSet(2);
     $scope.message = 'Hello';
     $scope.type = Problems.currentProblemSet();
   });
