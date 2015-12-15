35c5792ade8484de8b617dbc8ee51412a8bcc170
Ben Pochily <ben.pochily@gmail.com>
2015-12-14 21:10:26 -0500

    polymer binding not working, about to overhaul

--- client/app/s/s.controller.js
+++ client/app/s/s.controller.js
@@ -4,4 +4,9 @@ angular.module('hrr10MjbeApp')
   .controller('SCtrl', function ($scope, Problems) {
     $scope.message = 'Hello';
     $scope.type = Problems.currentProblemSet();
+    $scope.complete = 'false';
+    $scope.finish = function() {
+      console.log('finished');
+      console.log($scope.cor);
+    }
   });
