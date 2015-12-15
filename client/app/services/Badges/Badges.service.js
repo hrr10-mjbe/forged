7e56720677ed3486f15f5dffd43f4ab21ac2468c
Ben Pochily <ben.pochily@gmail.com>
2015-12-15 13:31:00 -0500

    scaffold badge service

---
+++ client/app/services/Badges/Badges.service.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('hrr10MjbeApp')
+  .service('Badges', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
---
+++ client/app/services/Badges/Badges.service.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Service: Badges', function () {
+
+  // load the service's module
+  beforeEach(module('hrr10MjbeApp'));
+
+  // instantiate service
+  var Badges;
+  beforeEach(inject(function (_Badges_) {
+    Badges = _Badges_;
+  }));
+
+  it('should do something', function () {
+    expect(!!Badges).to.be.true;
+  });
+
+});
