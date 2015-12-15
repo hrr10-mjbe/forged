84b1a29015b9cba365ed3a5bf52c0da3a303119b
Ben Pochily <ben.pochily@gmail.com>
2015-12-14 20:27:58 -0500

    remove tests

--- client/app/services/skills/skills.service.js
+++ client/app/services/skills/skills.service.js
@@ -1,67 +1,14 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .service('Skills', function($http, Auth, User) {
-    /*var skills;
-    //console.log(User.save);
-    console.log(Auth.getCurrentUser());
-    var user = Auth.getCurrentUser();
-    user.type = 'wooo';
-    User.save(user,
-        function(data) {
-          console.log(data);
-          var currentUser = User.get();
-          console.log(currentUser);
-        });*/
-Auth.getCurrentUser(null)
-        .then(function(user) {
-          console.log('this is the user ');
-          console.log(user);
-          console.log(user._id);
-          console.log(user);
-  console.log(user._id);
+  .service('Skills', function($http, User) {
 
- /* User.changePassword({ id: user._id }, {
-        oldPassword: 'nothing',
-        newPassword: 'morenothing'
-      }, function() {
-        //return safeCb(callback)(null);
-      }, function(err) {
-        //return safeCb(callback)(err);
-      }).$promise;*/
+    User.changeType({}, {
+      type: 'hohoho'
+    }, function() {
+      console.log(User.get());
+    }, function(err) {}).$promise;
 
-  User.changeType({}, {
-        type: 'bar'
-      }, function() {
-        console.log(User.get());
-      }, function(err) {
-      }).$promise;
-        });
-
- /*Auth.getCurrentUser(null).then(function(user) {
-  console.log(user);
-  console.log(user._id);
-
-  User.changePassword({ id: user._id }, {
-        oldPassword: 'nothing',
-        newPassword: 'morenothing'
-      }, function() {
-        //return safeCb(callback)(null);
-      }, function(err) {
-        //return safeCb(callback)(err);
-      }).$promise;
-
-  User.changeType({ id: user._id }, {
-        type: 'fooo'
-      }, function() {
-        console.log(User.get());
-        return safeCb(callback)(null);
-      }, function(err) {
-        return safeCb(callback)(err);
-      }).$promise;
-  )};*/
-        
-    //User.changePassword({ id: currentUser._id }
     this.getSkills = function(cb) {
       if (skills) {
         cb(skills);
