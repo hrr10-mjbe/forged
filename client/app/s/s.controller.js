240eb2cbfd2d791886fe4eb6ab48ff214c42c8bf
Ben Pochily <ben.pochily@gmail.com>
2015-12-14 22:40:12 -0500

    complete refactored controller, data binding works

--- client/app/s/s.controller.js
+++ client/app/s/s.controller.js
@@ -1,34 +1,19 @@
-/*'use strict';
-
-angular.module('hrr10MjbeApp')
-  .controller('SCtrl', function ($scope, Problems) {
-    $scope.message = 'Hello';
-    $scope.type = Problems.currentProblemSet();
-    $scope.complete = 'false';
-    $scope.finish = function() {
-      console.log('finished');
-      console.log($scope.complete);
-    };
-    $scope.go = function() {
-      console.log('going');
-      console.log($scope.userselction);
-    }
-  });*/
-
 'use strict';
 
 (function() {
 
-  class MainController {
-    constructor() {
-     
+  class SController {
+    constructor(Problems) {
+      this.complete = 'false';
+      this.type = Problems.currentProblemSet();
     }
     go() {
       console.log(this.correct);
+      console.log(this.complete);
     }
   }
 
   angular.module('hrr10MjbeApp')
-    .controller('SCtrl', MainController);
+    .controller('SCtrl', SController);
 
 })();
