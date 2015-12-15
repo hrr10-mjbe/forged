917a8dd7955e4d5cac738c9977d0d5ed97bb813a
Ben Pochily <ben.pochily@gmail.com>
2015-12-15 15:56:33 -0500

    sending JSON to polymer for badge display working

--- client/app/badges/badges.controller.js
+++ client/app/badges/badges.controller.js
@@ -3,4 +3,28 @@
 angular.module('hrr10MjbeApp')
   .controller('BadgesCtrl', function ($scope) {
     $scope.message = 'Hello';
+    $scope.badges = JSON.stringify([{name:'addition',
+                      image:'add',
+                      achieved: true},
+                      {name:'subtraction',
+                      image:'build',
+                      achieved: true},
+                      {name:'multiplication',
+                      image:'favorite',
+                      achieved: false},
+                      {name:'division',
+                      image:'account-balance',
+                      achieved: false},
+                      {name:'addition',
+                      image:'polymer',
+                      achieved: true},
+                      {name:'subtraction',
+                      image:'accessibility',
+                      achieved: true},
+                      {name:'multiplication',
+                      image:'assessment',
+                      achieved: false},
+                      {name:'division',
+                      image:'bookmark',
+                      achieved: false}]);
   });
