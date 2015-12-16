14b691d57c91de1c9af8797d28b7c29c1ef32447
Ben Pochily <ben.pochily@gmail.com>
2015-12-15 22:57:40 -0500

    badge display working

--- client/app/badges/badges.controller.js
+++ client/app/badges/badges.controller.js
@@ -1,11 +1,9 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .controller('BadgesCtrl', function ($scope, Badges) {
+  .controller('BadgesCtrl', function($scope, Badges) {
     $scope.message = 'Hello';
     Badges.getUserBadges(function(badges) {
       $scope.badges = badges;
     });
-    /*JSON.stringify([{name:'addition',
-                      image:'add'}]);*/
   });
