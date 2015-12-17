ff140fd6e87adb4ce03701ce52bfd0d6b2c524b7
Ben Pochily <ben.pochily@gmail.com>
2015-12-16 19:37:38 -0500

    rough accept invite function

--- client/app/services/Student/Student.service.js
+++ client/app/services/Student/Student.service.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .service('Student', function () {
+  .service('Student', function (Auth) {
+    this.acceptRequest = function(req) {
+      console.log(req);
+      $http({
+        method: 'POST',
+        url: '/api/users/accept',
+        data: {_id: req}
+      }).then(function successCallback(response) {
+        cb(response.status);
+      }, function errorCallback(response) {
+        cb(response.status);
+      });
+    }
     // AngularJS will instantiate a singleton by calling "new" on this function
   });
