e09aa057b4ad03aa762fa0d16550e05af33c829d
Ben Pochily <ben.pochily@gmail.com>
2015-12-16 19:06:11 -0500

    write teacher sendInvite

--- client/app/services/Teacher/Teacher.service.js
+++ client/app/services/Teacher/Teacher.service.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .service('Teacher', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+  .service('Teacher', function($http) {
+    this.sendInvite = function(email, cb) {
+      $http({
+        method: 'POST',
+        url: '/api/users/invite',
+        data: email
+      }).then(function successCallback(response) {
+        console.log(response.data);
+        cb(response.data);
+      }, function errorCallback(response) {
+        console.log(response);
+      });
+    }
   });
