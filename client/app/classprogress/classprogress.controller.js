44727ec98d434d7949ebc48b242afed832a8b636
Ben Pochily <ben.pochily@gmail.com>
2015-12-19 18:33:23 -0500

    cleanup

--- client/app/classprogress/classprogress.controller.js
+++ client/app/classprogress/classprogress.controller.js
@@ -1,12 +1,10 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .controller('ClassprogressCtrl', function ($scope, $stateParams, Teacher) {
+  .controller('ClassprogressCtrl', function($scope, $stateParams, Teacher) {
     $scope.message = 'Hello';
     Teacher.getClass($stateParams.id, function(theClass) {
       $scope.name = theClass.name;
       $scope.data = theClass.students;
-      console.log($scope.data);
-      console.log(theClass);
     })
   });
