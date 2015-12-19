84d41baf5e106d7ce28d0ea0792dddb5023fbdd1
Ben Pochily <ben.pochily@gmail.com>
2015-12-19 14:50:07 -0500

    toggle sign in/sign out mostly working

--- client/app/Navbar/Navbar.controller.js
+++ client/app/Navbar/Navbar.controller.js
@@ -1,17 +1,15 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .controller('NavbarCtrl', function ($rootScope, $scope, Auth) {
+  .controller('NavbarCtrl', function($rootScope, $scope, Auth) {
     $scope.message = 'Hello';
     Auth.isLoggedIn(function(is) {
       $scope.loggedIn = is;
     })
- // $scope.loggedIn = Auth.isLoggedIn();
 
-  $rootScope.$on('$stateChangeStart', 
-function(event, toState, toParams, fromState, fromParams){ 
-    Auth.isLoggedIn(function(is) {
-      $scope.loggedIn = is;
+    $rootScope.$on('$stateChangeStart', function() {
+      Auth.isLoggedIn(function(is) {
+        $scope.loggedIn = is;
+      })
     })
-})
   });
