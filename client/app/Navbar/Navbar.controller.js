08277e947fbecda8f424d7622be5e24f8afff4f8
Ben Pochily <ben.pochily@gmail.com>
2015-12-19 14:45:41 -0500

    show/hide sign out working

--- client/app/Navbar/Navbar.controller.js
+++ client/app/Navbar/Navbar.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('hrr10MjbeApp')
-  .controller('NavbarCtrl', function ($scope) {
+  .controller('NavbarCtrl', function ($rootScope, $scope, Auth) {
     $scope.message = 'Hello';
+    Auth.isLoggedIn(function(is) {
+      $scope.loggedIn = is;
+    })
+ // $scope.loggedIn = Auth.isLoggedIn();
+
+  $rootScope.$on('$stateChangeStart', 
+function(event, toState, toParams, fromState, fromParams){ 
+    Auth.isLoggedIn(function(is) {
+      $scope.loggedIn = is;
+    })
+})
   });
