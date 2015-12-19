31e3013c94e7626ba14eb3b879401f30aaff6348
Ben Pochily <ben.pochily@gmail.com>
2015-12-19 16:20:00 -0500

    remove extra code, cleanup

--- client/app/Navbar/Navbar.controller.js
+++ client/app/Navbar/Navbar.controller.js
@@ -2,30 +2,15 @@
 
 angular.module('hrr10MjbeApp')
   .controller('NavbarCtrl', function($rootScope, $scope, Auth) {
-    $scope.message = 'Hello';
-    Auth.isLoggedIn(function(is) {
-      $scope.loggedIn = is;
-     Auth.getCurrentUser();
-    })
-
-    Auth.getCurrentUser(function(user) {
-      $scope.name = user.name;
-    })
-
-    Auth.isTeacher(function(is) {
-      $scope.isTeacher = is;
-    })
-
     $rootScope.$on('$stateChangeStart', function() {
       Auth.isLoggedIn(function(is) {
         $scope.loggedIn = is;
       })
       Auth.isTeacher(function(is) {
-      $scope.isTeacher = is;
-    })
-       Auth.getCurrentUser(function(user) {
-      $scope.name = user.name;
-    })
-
+        $scope.isTeacher = is;
+      })
+      Auth.getCurrentUser(function(user) {
+        $scope.name = user.name;
+      })
     })
   });
