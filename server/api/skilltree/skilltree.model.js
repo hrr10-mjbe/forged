69b81fb75194b7857e44cbe39c611a13c42cb898
Ben Pochily <ben.pochily@gmail.com>
2015-12-19 19:53:45 -0500

    write skilltree schema, begin seed data

--- server/api/skilltree/skilltree.model.js
+++ server/api/skilltree/skilltree.model.js
@@ -1,12 +1,23 @@
 'use strict';
 
 var mongoose = require('bluebird').promisifyAll(require('mongoose'));
+var tree = require('mongoose-tree');
 var Schema = mongoose.Schema;
 
 var SkilltreeSchema = new Schema({
   name: String,
   info: String,
-  active: Boolean
+  active: Boolean,
+  skills: [{type: Schema.Types.ObjectId, ref: 'Skill'}]
 });
 
+SkilltreeSchema.plugin(tree);
+
 module.exports = mongoose.model('Skilltree', SkilltreeSchema);
+
+/*----------------------------------------------------------
+Seed data:*/
+var beginningMath = new module.exports({name: beginningMath});
+
+
+//---------------------------------------------------*/
