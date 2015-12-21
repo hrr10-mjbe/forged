779783ff2ff0042eaeb8a95dc5c4cd9c627ccf12
Ben Pochily <ben.pochily@gmail.com>
2015-12-20 21:09:59 -0500

    update skill seed data

--- server/api/defaultuser/defaultuser.controller.js
+++ server/api/defaultuser/defaultuser.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 var User = require('../user/user.model');
+var Skilltree = require('../skilltree/skilltree.model');
 
 exports.defaultUser = function(req, res, next) {
   var user = new User();
+  Skilltree.findOneAsync({name: })
   res.json(user);
 }
